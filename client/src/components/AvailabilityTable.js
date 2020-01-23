8fbe28c5671e09beda20b5a0477d03aa8468626c
BenedyktT <benedykt.tyminski@gmail.com>
2020-01-23 23:04:45 +0000

    working at contextapi

--- client/src/components/AvailabilityTable.js
+++ client/src/components/AvailabilityTable.js
@@ -2,39 +2,38 @@ import React from "react";
 import { CalendarConsumer } from "../context";
 
 const AvailabilityTable = () => {
-	return (
-		<CalendarConsumer>
-			{value => {
-				console.log(value);
-				return (
-					<div className="table-wrapper container">
-						<table className="blueTable">
-							<thead>
-								<tr>
-									<th></th>
-									<th>Availability</th>
-									<th>Rate</th>
-								</tr>
-							</thead>
+  return (
+    <CalendarConsumer>
+      {({ startDate, endDate }) => {
+        return (
+          <div className="table-wrapper container">
+            <table className="blueTable">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Availability</th>
+                  <th>Rate</th>
+                </tr>
+              </thead>
 
-							<tbody>
-								<tr>
-									<td>Economy</td>
-									<td>19000</td>
-									<td>15000</td>
-								</tr>
-								<tr>
-									<td>Standard</td>
-									<td>13000</td>
-									<td>12500</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
-				);
-			}}
-		</CalendarConsumer>
-	);
+              <tbody>
+                <tr>
+                  <td>Economy</td>
+                  <td>19000</td>
+                  <td>15000</td>
+                </tr>
+                <tr>
+                  <td>Standard</td>
+                  <td>13000</td>
+                  <td>12500</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        );
+      }}
+    </CalendarConsumer>
+  );
 };
 
 export default AvailabilityTable;
--- client/src/context/index.js
+++ client/src/context/index.js
@@ -1,30 +1,63 @@
 import moment from "moment";
 import React from "react";
-
+import axios from "axios";
 const Context = React.createContext();
 export const reducer = (state, action) => {
-	const { payload, type } = action;
-	switch (type) {
-		case "SET_CALENDAR":
-			return { ...state, payload };
+  const { payload, type } = action;
+  switch (type) {
+    case "FOCUS_INPUT":
+      return {
+        ...state,
+        focusedInput: payload.focusedInput
+      };
+    case "SET_CALENDAR":
+      const { startDate, endDate } = payload;
+      return { ...state, startDate, endDate };
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 export class CalendarState extends React.Component {
-	state = {
-		startDate: moment(),
-		endDate: moment().add(1, "day"),
-		focused: false,
-		dispatch: action => {
-			this.setState(state => reducer(state, action));
-		}
-	};
-	render() {
-		const { children } = this.props;
-		return <Context.Provider value={this.state}>{children}</Context.Provider>;
-	}
+  state = {
+    startDate: moment(),
+    endDate: moment().add(1, "day"),
+    focusedInput: null,
+    test: null,
+    dispatch: action => {
+      this.setState(state => reducer(state, action));
+    }
+  };
+
+  // nie dziala warunek component updatuje sie czy end date null co powoduje error :/
+
+  /* async componentDidUpdate(prevProps, prevState) {
+    if (
+      !moment(prevState.startDate).isSame(this.state.startDate, "day") ||
+      (!moment(prevState.endDate).isSame(this.state.endDate, "day") &&
+        typeof this.state.startDate === "object" &&
+        typeof this.state.endDate === "object")
+    ) {
+      try {
+        let { startDate, endDate } = this.state;
+        startDate = moment(startDate).format("YYYY-MM-DD");
+        endDate = moment(endDate).format("YYYY-MM-DD");
+        const res = await axios.get(
+          `/availability/${startDate}/${endDate}/LAKI`
+        );
+
+        this.setState(() => ({ test: res.data }));
+        console.log(this.state.test);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  } */
+
+  render() {
+    const { children } = this.props;
+    return <Context.Provider value={this.state}>{children}</Context.Provider>;
+  }
 }
 
 export const CalendarConsumer = Context.Consumer;
