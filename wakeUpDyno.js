0739c356fcd5bc4aeda912a886710eb68dc0bd8b
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-02-03 20:47:02 +0000

    function import to wrong file bugfix

--- wakeUpDyno.js
+++ wakeUpDyno.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const wakeUpDyno = require("./wakeUpDyno");
 
 /*
 If current time falls between startTime and endTime, returns length startTime and endTime are arrays of numbers representing the time of day. They follow this pattern: [Hours, Minutes, Seconds, Milliseconds]. If endTime is less than startTime time, endTime will be assumed to be on the following day. 
