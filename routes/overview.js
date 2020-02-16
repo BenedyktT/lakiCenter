ee04ff79c40319fbeb99733de858c82316fc9933
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-02-16 08:10:17 +0000

    samplereq

--- routes/overview.js
+++ routes/overview.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const axios = require("axios");
 const auth = require("../middleware/auth");
 const apiAuth = require("../middleware/apiAuth");
+const sample = require("./sample.json");
 
-router.post("/", apiAuth, async (req, res) => {
+router.get("/", apiAuth, async (req, res) => {
 	try {
 		/* 	 const res = await axios.get(
 			"services/bookingapi/reservations?stayFromDate=2020-02-13&stayToDate=2020-02-14&includeOutOfOrder=false&includeInvoices=false&modifiedSince=2020-02-13T15:55:32"
@@ -19,10 +20,8 @@ router.post("/", apiAuth, async (req, res) => {
 			}
 		); */
 
-		const xml = {
-			NewBookingRequest: {}
-		};
-		const response = await axios.post("/roomer/openAPI/REST/bookings", json, {
+		const xml = sample;
+		const response = await axios.post("/roomer/openAPI/REST/bookings", xml, {
 			headers: {
 				"Content-Type": "application/json",
 				Accept: "application/json"
